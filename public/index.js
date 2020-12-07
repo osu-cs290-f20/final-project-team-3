74e2e942abd1a2ef94532151148887af13163419
NawafAlOthman <72106212+NawafAlOthman@users.noreply.github.com>
2020-12-06 21:46:46 -0800

    html xp works (not the handlebars tho)

--- public/index.js
+++ public/index.js
@@ -1,24 +1,30 @@
 let level = 0;
-let maxExp = 500;
-let currExp = 0;
+
+let progressBar = document.getElementById('progress-bar');
+let levelTracker = document.getElementById('current-level');
+
+let maxExp = progressBar.max;
+let currExp = progressBar.value;
 
 
-function insertNewPost(level, maxExp, currExp) {
+// function insertNewPost(level, maxExp, currExp) {
 
-    let context = {
-      level: level,
-      maxExp: maxExp,
-      currExp: currExp,
-    }
+//     let context = {
+//       level: level,
+//       maxExp: maxExp,
+//       currExp: currExp,
+//     }
   
-    let info = Handlebars.templates.userInfo(context);
+//     let info = Handlebars.templates.userInfo(context);
   
-    var header = document.getElementById('header-info');
-    header.insertAdjacentHTML('beforeend', info);
-    return info;
+//     var header = document.getElementById('header-info');
+//     header.insertAdjacentHTML('beforeend', info);
+//     return info;
   
-  }
-  insertNewPost(level, maxExp, currExp);
+//   }
+//   insertNewPost(level, maxExp, currExp);
+
+
   let questListener = document.getElementById('add-quest-button');
 
 function addExp(){
@@ -30,7 +36,11 @@ function addExp(){
     maxExp = maxExp+150;
   }
 
-  //insertNewPost(level, maxExp, currExp);
+ progressBar.max = maxExp;
+ progressBar.value = currExp;
+ levelTracker.innerHTML = level;
+
+
 }
 
   questListener.addEventListener('click', addExp);
