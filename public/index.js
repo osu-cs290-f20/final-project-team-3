db2257c58cb8daf6d80813601d086ee463c648f1
dodsonta <71948722+dodsonta@users.noreply.github.com>
2020-12-06 16:00:03 -0800

    good way through batabases for quests

--- public/index.js
+++ public/index.js
@@ -2,29 +2,54 @@ var hidden = document.getElementsByClassName('hidden');
 var exitAddQuest = document.getElementsByClassName('addQuest-hide-button');
 var addQuest = document.getElementById('addQuest-add');
 
-function rehideAddQuest(event){
+function rehideAddQuest(){
 	/*for(var i = 0; i < hidden.length; i++){
 		hidden[i].style.display = "none";
 	}*/
 	document.getElementById('addQuest-title-inp').value = "";
 	document.getElementById('addQuest-desc-inp').value = ""; 
-	document.getElementById('addQuest-date-inp').value = "yyyy-MM-dd"; //Possible errors later, we'll see
+	document.getElementById('addQuest-date-inp').value = "";
 }
 
 function addQuest_DB(title, desc, date){
 	var request = new XMLHttpRequest();
+	var requestURL = '/';
+	request.open('POST', requestURL);
+
+	var quest = {
+		title: title, 
+		desc: desc, 
+		date: date
+	};
+	var reqBody = JSON.stringify(quest);
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.addEventListener('load', function(event){
+		if(event.target.status == 200){
+			//Code for adding onto client
+		}
+		else
+			window.alert('Error adding quest:' + event.target.response);
+	})
+	request.send(reqBody);
 }
 
 for(var i=0; i < exitAddQuest.length; i++)
 	exitAddQuest[i].addEventListener('click', rehideAddQuest);
 
-addQuest.addEventListener('click', function{
+addQuest.addEventListener('click', function(){
 	var title = document.getElementById('addQuest-title-inp').value.trim();
 	var desc = document.getElementById('addQuest-desc-inp').value.trim(); 
 	var date = document.getElementById('addQuest-date-inp').value.trim();
 
-	console.log('Title:', title);
-	console.log('Description:', desc);
-	console.log('Date:', date);
-}
+	// console.log('Title:', title);
+	// console.log('Description:', desc);
+	// console.log('Date:', date);
 
+	if(title == '' || desc == '' || date == ''){
+		window.alert("You didn't enter all info");
+	}
+	else{
+		addQuest_DB(title, desc, date);
+		rehideAddQuest();
+	}
+});
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var handlebars = require('handlebars');
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+//var path = require('path'); Maybe won't need
+
+var questsData = require('./peopleData');
 
 var app = express();
 var port = process.env.PORT || 3459;
@@ -10,3 +14,19 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/', function(req, res, next){
+	res.render('../public/index')
+})
+
+app.post('/', function(req, res){
+	if(req.body && req.body.title && req.body.desc && req.body.date){
+		fs.writeFile(
+			__dirname + '/questsData.JSON',
+			JSON.stringify(peopleData, null, 2)
+		)
+		res.status(200).send("Quest added succesfully");
+	}
+	else
+		res.status(400).send("Error with data");
+})
